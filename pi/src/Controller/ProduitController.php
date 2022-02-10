cd507fdeee08f198aa6a86b17b7a5de6a900b368
Mahmoud Cheikh <mahmoud.cheikh@esprit.tn>
2022-02-10 14:52:55 +0100

    Update ProduitController.php

--- pi/src/Controller/ProduitController.php
+++ pi/src/Controller/ProduitController.php
@@ -12,12 +12,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/Produit")
+ * @Route("/produit")
  */
 class ProduitController extends AbstractController
 {
     /**
-     * @Route("/", name="Produit_index", methods={"GET"})
+     * @Route("/", name="produit_index", methods={"GET"})
      */
     public function index(ProduitRepository $ProduitRepository): Response
     {
@@ -27,7 +27,7 @@ class ProduitController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="Produit_new", methods={"GET", "POST"})
+     * @Route("/new", name="produit_new", methods={"GET", "POST"})
      */
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -49,7 +49,7 @@ class ProduitController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="Produit_show", methods={"GET"})
+     * @Route("/{id}", name="produit_show", methods={"GET"})
      */
     public function show(Produit $Produit): Response
     {
@@ -59,7 +59,7 @@ class ProduitController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="Produit_edit", methods={"GET", "POST"})
+     * @Route("/{id}/edit", name="produit_edit", methods={"GET", "POST"})
      */
     public function edit(Request $request, Produit $Produit, EntityManagerInterface $entityManager): Response
     {
@@ -79,7 +79,7 @@ class ProduitController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="Produit_delete", methods={"POST"})
+     * @Route("/{id}", name="produit_delete", methods={"POST"})
      */
     public function delete(Request $request, Produit $Produit, EntityManagerInterface $entityManager): Response
     {
