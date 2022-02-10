b0558482612daef0ae5968f49614fe9f48eb9c05
Mahmoud Cheikh <mahmoud.cheikh@esprit.tn>
2022-02-10 15:16:25 +0100

    Update EvenementController.php

--- pi/src/Controller/EvenementController.php
+++ pi/src/Controller/EvenementController.php
@@ -12,22 +12,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/Evenement")
+ * @Route("/evenement")
  */
 class EvenementController extends AbstractController
 {
     /**
-     * @Route("/", name="Evenement_index", methods={"GET"})
+     * @Route("/", name="evenement_index", methods={"GET"})
      */
     public function index(EvenementRepository $EvenementRepository): Response
     {
         return $this->render('Evenement/index.html.twig', [
-            'Evenements' => $EvenementRepository->findAll(),
+            'evenements' => $EvenementRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/new", name="Evenement_new", methods={"GET", "POST"})
+     * @Route("/new", name="evenement_new", methods={"GET", "POST"})
      */
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -39,27 +39,27 @@ class EvenementController extends AbstractController
             $entityManager->persist($Evenement);
             $entityManager->flush();
 
-            return $this->redirectToRoute('Evenement_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('evenement_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('Evenement/new.html.twig', [
-            'Evenement' => $Evenement,
+            'evenement' => $Evenement,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="Evenement_show", methods={"GET"})
+     * @Route("/{id}", name="evenement_show", methods={"GET"})
      */
     public function show(Evenement $Evenement): Response
     {
         return $this->render('Evenement/show.html.twig', [
-            'Evenement' => $Evenement,
+            'evenement' => $Evenement,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="Evenement_edit", methods={"GET", "POST"})
+     * @Route("/{id}/edit", name="evenement_edit", methods={"GET", "POST"})
      */
     public function edit(Request $request, Evenement $Evenement, EntityManagerInterface $entityManager): Response
     {
@@ -69,17 +69,17 @@ class EvenementController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('Evenement_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('evenement_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('Evenement/edit.html.twig', [
-            'Evenement' => $Evenement,
+            'evenement' => $Evenement,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="Evenement_delete", methods={"POST"})
+     * @Route("/{id}", name="evenement_delete", methods={"POST"})
      */
     public function delete(Request $request, Evenement $Evenement, EntityManagerInterface $entityManager): Response
     {
@@ -88,6 +88,6 @@ class EvenementController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('Evenement_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('evenement_index', [], Response::HTTP_SEE_OTHER);
     }
 }
